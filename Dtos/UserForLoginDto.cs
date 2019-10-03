a53ecb21b624fcd91e797a1130280a2a3381f321
seunnuga93 <seunnuga93@gmail.com>
2019-10-03 16:49:34 +0100

    Authentication

---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DatingApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
